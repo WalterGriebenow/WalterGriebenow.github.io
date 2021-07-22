385cd2afff377576651b9a8853195290c77c2055
WalterGriebenow <wfg2@illinois.edu>
2021-07-22 02:42:05 -0500

    aafasf

--- main.js
+++ main.js
@@ -3,32 +3,35 @@
 *    Mastering Data Visualization with D3.js
 *    2.5 - Activity: Adding SVGs to the screen
 */
-const svg = d3.select("#chart-area").append("svg")
-    .attr("width", 400)
-    .attr("height", 400)
-    .attr("style", "outline: thin solid red;")
-
-svg.append("circle")
-    .attr("cx",100)
-    .attr("cy",250)
-    .attr("r",70)
-    .attr("fill","red")
+<body onload = 'init()'>
 
 async function init() {
     data = await d3.csv("https://raw.githubusercontent.com/alex6499cat/CS416-NarrativeVisualization/main/cars2017(1).csv");
+    
+    const svg = d3.select("#chart-area").append("svg")
+    .attr("width", 400)
+    .attr("height", 400)
+    .attr("style", "outline: thin solid black;")
+
+    svg.append("circle")
+    .attr("cx",100)
+    .attr("cy",250)
+    .attr("r",70)
+    .attr("fill","blue")
+    
     var x = d3.scaleLog().base(10).domain([10,150]).range([0,200]);
-var y = d3.scaleLog().base(10).domain([10,150]).range([200,0]);
+    var y = d3.scaleLog().base(10).domain([10,150]).range([200,0]);
 
-var axisx = d3.axisBottom(x).tickValues([10,20,50,100]).tickFormat(d3.format("~s"));
-var axisy = d3.axisLeft(y).tickValues([10,20,50,100]).tickFormat(d3.format("~s"));
+    var axisx = d3.axisBottom(x).tickValues([10,20,50,100]).tickFormat(d3.format("~s"));
+    var axisy = d3.axisLeft(y).tickValues([10,20,50,100]).tickFormat(d3.format("~s"));
 
-d3.select("svg").append("g").attr("transform","translate(50,50)")
-  .selectAll("dot")
-  .data(data).enter().append("circle")
-     .attr("cx", function (d,i) {return x(d.AverageCityMPG);}) 
-     .attr("cy", function (d,i) {return y(d.AverageHighwayMPG);})
-     .attr("r", function(d,i) {return d.EngineCylinders*1 +2;});
+    svg.append("g").attr("transform","translate(50,50)")
+    .selectAll("dot")
+    .data(data).enter().append("circle")
+    .attr("cx", function (d,i) {return x(d.AverageCityMPG);}) 
+    .attr("cy", function (d,i) {return y(d.AverageHighwayMPG);})
+    .attr("r", function(d,i) {return d.EngineCylinders*1 +2;});
 
-d3.select("svg").append("g").attr("transform","translate(50,50)").call(axisy);
-d3.select("svg").append("g").attr("transform","translate(50,250)").call(axisx);
+    svg.append("g").attr("transform","translate(50,50)").call(axisy);
+    svg.append("g").attr("transform","translate(50,250)").call(axisx);
 }
