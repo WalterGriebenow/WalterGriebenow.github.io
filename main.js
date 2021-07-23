5346187c2780d578b71e1e31455ec82f3598664b
WalterGriebenow <wfg2@illinois.edu>
2021-07-23 00:52:49 -0500

    aaaaaa

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 */
 
 const svg = d3.select("#chart-area").append("svg")
-.attr("width", 300)
+.attr("width", 500)
 .attr("height", 300)
 .attr("style", "outline: thin solid black;");
 
@@ -22,19 +22,19 @@ async function init() {
     const data = await d3.csv("Peru Elections "+ year +".csv");
     console.log(data);
     
-    var x = d3.scaleBand().domain([0,1,2,3,4,5,6,7,8,9]).range([0,200]);
+    var x = d3.scaleBand().domain([0,1,2,3,4,5,6,7,8,9]).range([0,400]);
     var y = d3.scaleLinear().domain([0,70]).range([200,0]);
 
     var axisx = d3.axisBottom(x);
-    var axisy = d3.axisLeft(y).tickValues([10,20,30,40,50,60]);
+    var axisy = d3.axisLeft(y).tickValues([10,20,30,40,50,60,70]);
 
     svg.append("g").attr("transform","translate(50,50)")
     .selectAll("dot")
     .data(data).enter().append("rect")
     .attr("x", function (d,i) {return x(i);}) 
-    .attr("y", function (d,i) {return y(+d.Votes1);})
-    .attr("width", function(d,i) {return x.bandwidth()-1;})
-    .attr("height",function (d) {return 200 - y(+d.Votes1)})
+    .attr("y", function (d,i) {return y(+d.Votes1*1);})
+    .attr("width", function(d,i) {return x.bandwidth()-2;})
+    .attr("height",function (d) {return 200 - y(+d.Votes1*1)})
     .attr("fill","blue");
 
     svg.append("g").attr("transform","translate(50,50)").call(axisy);
